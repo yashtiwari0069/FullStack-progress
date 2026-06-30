5475a22df8440c0c1a7683e5bd0a18a9602a0316
yashtiwari0069 <yt5160359@gmail.com>
2026-06-30 15:26:07 +0530

    day20  completed

---
+++ WebDev/day20/script2.js
@@ -0,0 +1,37 @@
+// var a = "yash"
+// var b = 22
+var c = 3.55
+var d = undefined
+var e = null
+var f =true
+
+// console.log(typeof a , typeof b, typeof c, typeof d, typeof d, typeof e)
+// typeof = is used to tell the datatype of data stored in the variable
+// [IMP]interview question = y datatype of null is object
+
+// generally we use let bcuz var is a global variable
+// example- 
+
+let a= 22
+{
+    let a = 33
+    console.log(a)
+}
+console.log(a)
+// this phenomenon cant happen using var
+
+const day ="tuesday"
+// const = means the data stored in the var is fixed
+
+
+let student ={
+    "name":"yash",
+    "age": 21
+
+}
+// creating student objects
+console.log(student)
+student.marks = 50
+// adding attributes
+console.log(student)
+
