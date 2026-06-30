a95776b4ec73f9a1f9e450f8b51de14aa6fdb6e5
yashtiwari0069 <yt5160359@gmail.com>
2026-06-30 14:48:35 +0530

    day20  completed

---
+++ WebDev/day20/script.js
@@ -0,0 +1,26 @@
+alert("so mat uth ja!!!!")
+// use when u want the user attention by stopping whatever he is doing
+
+console.log("my name is yash tiwari")
+// anything inside () is visible to the user via screen
+// usally used to veiw the output
+
+var a = prompt("enter the current time")
+// taking input in alert
+
+var isTrue = confirm("are u sure u want to sleep more")
+if(isTrue){
+    console.log("u are cooked")
+}
+else{
+    console.log('u are not cooked')
+}
+
+// document.title="lol"
+// changes the title of the webpage
+
+// document.body.style.backgroundColor="red"
+
+// js is used for frontend as well as backend using node.js
+// it make website interactive
+// it was just a introduction
